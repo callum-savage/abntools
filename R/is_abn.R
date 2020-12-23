40999b3df2b533857bfe8f13ad2fe040763b6d21
callum-savage <callum.s.savage@gmail.com>
2020-12-23 21:52:44 +1100

    Update is_abn documentation, and replace argument with x (instead of abns)

--- R/is_abn.R
+++ R/is_abn.R
@@ -1,26 +1,31 @@
-#' Use a checksum to identify if an ABN is valid.
+#' Use a checksum to identify if a number is in the format of an ABN.
 #'
-#' @param abns A numeric or character vector of ABNs
+#' This function identifies if a number is in the format of a valid ABN, using
+#' the rules oultined [here](https://abr.business.gov.au/Help/AbnFormat). Note
+#' that this function does not confirm that the ABN is valid; it simply checks
+#' if it *could* be valid.
 #'
-#' @return A logical vector of the same length as abns identifying if each
-#'     element is a valid ABN
+#' @param x A numeric or character vector of numbers to check
+#'
+#' @return A logical vector of the same length as \code{x} identifying if
+#'   each element is in the format of a valid ABN
 #' @export
 #'
 #' @examples
-#'     is_abn(19621994018)
-is_abn <- function(abns) {
+#' is_abn(19621994018)
+is_abn <- function(x) {
 
-  if (is.list(abns)) {
-    abns <- unlist(abns)
+  if (is.list(x)) {
+    x <- unlist(x)
   }
 
-  if (!is.character(abns)) {
-    abns <- as.character(abns)
+  if (!is.character(x)) {
+    x <- as.character(x)
   }
 
-  abns <- gsub(" ", "", abns, fixed = TRUE)
-  abns[nchar(abns, keepNA = FALSE) != 11] <- "00000000000"
-  abn_matrix <- simplify2array(lapply(strsplit(abns, "", fixed = TRUE), as.numeric))
+  x <- gsub(" ", "", x, fixed = TRUE)
+  x[nchar(x, keepNA = FALSE) != 11] <- "00000000000"
+  abn_matrix <- simplify2array(lapply(strsplit(x, "", fixed = TRUE), as.numeric))
   abn_sums <- colSums(abn_matrix * c(10, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19))
   checksums <- (abn_sums - 10) %% 89
   checksums == 0
