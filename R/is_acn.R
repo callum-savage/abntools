62b4a1657f04691cb66c88af25726a5ac23d6e37
callum-savage <callum.s.savage@gmail.com>
2020-12-23 21:55:39 +1100

    Create is_acn function

---
+++ R/is_acn.R
@@ -0,0 +1,17 @@
+is_acn <- function(x) {
+
+  if (is.list(x)) {
+    acns <- unlist(x)
+  }
+
+  if (!is.character(x)) {
+    x <- as.character(x)
+  }
+
+  x <- gsub(" ", "", x, fixed = TRUE)
+  x[nchar(x, keepNA = FALSE) != 9] <- "000000001"
+  acn_matrix <- simplify2array(lapply(strsplit(acns, "", fixed = TRUE), as.numeric))
+  acn_sums <- colSums(acn_matrix * c(8, 7, 6, 5, 4, 3, 2, 1, 1))
+  checksums <- (acn_sums %% 10)
+  checksums == 0
+}
