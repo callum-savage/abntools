a819f7ffb6f9c2ce50893d58a610f2deb5fe9cc9
callum-savage <callum.s.savage@gmail.com>
2021-01-05 20:12:20 +1100

    Write a package documentation starter

---
+++ R/abntools.R
@@ -0,0 +1,13 @@
+#' abntools: A package for working with ABNs and ACNs
+#'
+#' The abntools package provides tools to work with Australian Business Numbers
+#' (ABNs) and Australian Company Numbers (ACNs). The most important of these
+#' tools is an interface with the ABN lookup API.
+#'
+#' @section Validation functions:
+#' The functions \code{is_abn} and \code{is_acn} are provided to determine if an
+#' input is in the format of a valid ABN or ACN respectively.
+#'
+#' @docType package
+#' @name abntools
+NULL
